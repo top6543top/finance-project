14e880a53d7c7a12f27d536191a926e8e30efb01
top6543top <top6543top@gamil.com>
2026-04-25 01:04:31 +0900

    [feat] Member 엔티티 and 도메인 구현

---
+++ src/main/java/com/trading/account/domain/member/Member.java
@@ -0,0 +1,29 @@
+package com.trading.account.domain.member;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
